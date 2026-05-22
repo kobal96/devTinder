953df43f977b80c3b616d307813f4918ce5df7e2
Shiv <shiv@Shivs-MacBook-Air.local>
2026-05-22 14:18:06 +0530

    changes done related to user module

--- routes/request.js
+++ routes/request.js
@@ -14,12 +14,19 @@ requestconnect.post(
       // const { status, userId } = req.params;
       // Your logic here to handle the connection request based on the status and userId
       const validStatuses = ["interested", "ignore", "reject", "accepted"];
+      
+    
+      
       if (!validStatuses.includes(req.params.status)) {
         return res.status(400).json({ message: "Invalid status value" });
       }
       const senderUserId = req.user._id; // Assuming you have user authentication middleware that sets req.user
       const receiverUserId = req.params.userId;
       const status = req.params.status;
+    
+      if(senderUserId.toString() === receiverUserId){
+        return res.status(400).json({ message: "You cannot send a connection request to yourself" });
+      }
       // Implement your logic to save the connection request in the database
       // For example, you might have a ConnectionRequest model to handle this
       // await ConnectionRequest.create({ sender: senderUserId, receiver: receiverUserId, status });
@@ -28,6 +35,7 @@ requestconnect.post(
       if (!receiverUser) {
         return res.status(404).json({ message: "Receiver user not found" });
       }
+
       const senderUser = await User.findById(senderUserId);
       if (!senderUser) {
         return res.status(404).json({ message: "Sender user not found" });
@@ -50,14 +58,10 @@ requestconnect.post(
         status,
       });
       const data = await connectionRequest.save();
-      // const emailRes = await sendEmail.run(
-      //   "A new friend request from " + req.user.firstName,
-      //   req.user.firstName + " is " + status + " in " + toUser.firstName
-      // );
-      // console.log(emailRes);
+      
       res.json({
         message:
-          req.user.firstName + " is " + status + " in " + senderUser.firstName,
+          req.user.firstName + " is " + status + " in " + receiverUser.firstName,
         data,
       });
       res.json({ message: "Connection request processed successfully" });
@@ -67,4 +71,29 @@ requestconnect.post(
     }
   },
 );
+
+requestconnect.post("/api/request/review/:status/:connectionRequestId", authUser, async (req, res) => {
+    try {
+        const validStatuses = ["rejected", "accepted"];
+        if (!validStatuses.includes(req.params.status)) {
+            return res.status(400).json({ message: "Invalid status value" });
+        }
+        const connectionRequestId = req.params.connectionRequestId;
+        const receiverUserId = req.user._id;
+        const status = req.params.status;
+        
+        // get the connectionRequestId user data 
+        const condition = { _id: connectionRequestId, receiverUserId,status: "interested" };
+        const connectionRequest = await ConnectionRequest.findOne(condition);
+        if (!connectionRequest) {
+            return res.status(404).json({ message: "Connection request not found or already reviewed"});
+        }
+        connectionRequest.status = status;
+        await connectionRequest.save();
+        res.json({ message: "Connection request reviewed successfully", connectionRequest });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+});
+
 export default requestconnect;
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,59 @@
 import express from "express";
 const userrouter = express.Router();
 import User from "../models/users.js";
+import ConnectionRequest from "../models/connectionRequest.js";
 import UserAuth from "../middleware/auth.js";
 import bcrypt from "bcryptjs";
 
-userrouter.get("/api/users", UserAuth, async (req, res) => {
+userrouter.get("/api/users/connectionRequested", UserAuth, async (req, res) => {
   try {
-    const users = await User.find();
-    res.json(users);
+    const userId = req.user._id;
+    const requstedConnections = await ConnectionRequest.find({
+      receiverUserId: userId,
+      status: "interested",
+    }).populate("senderUserId", "firstName lastName email profilePicture");
+    res.json({ requestedConnections: requstedConnections });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
-}); 
+});
+
+
+userrouter.get("/api/users/connected", UserAuth, async (req, res) => {
+    try {
+        const condition = {
+            $or: [
+                { senderUserId: req.user._id, status: "accepted" },
+                { receiverUserId: req.user._id, status: "accepted" },
+            ],
+        };
+        const connections = await ConnectionRequest.find(condition)
+            .populate("senderUserId", "firstName lastName  profilePicture")
+            .populate("receiverUserId", "firstName lastName  profilePicture");
+
+            if(connections.length === 0){
+                return res.status(404).json({ message: "No connections found" });
+            }
+
+            console.log("Connections found:", connections[0].senderUserId._id);
+            // return false÷;
+            var connectedUser;
+            if(connections[0].senderUserId._id.toString() === req.user._id.toString()){
+
+                connectedUser = connections[0].receiverUserId;
+            }else{
+                // throw new Error("Unexpected connection data structure÷");
+                connectedUser = connections[0].senderUserId;
+            }
+
+        res.json({ connectedUser });  
+
+
+
+    } catch (error) {
+      res.status(500).json({ message: "Server error", error: error.message });
+    }
+  });
 
 
 
